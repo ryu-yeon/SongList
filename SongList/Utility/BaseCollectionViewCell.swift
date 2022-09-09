5baec40eef8142fb22aebd19147cc8a83bbb35ca
ryu-yeon <rytak108@gmail.com>
2022-09-10 01:52:59 +0900

    utility BaseCollectionViewCell 생성

--- SongList/Utility/BaseCollectionViewCell.swift
+++ SongList/Utility/BaseCollectionViewCell.swift
@@ -10,9 +10,13 @@ import UIKit
 class BaseCollectionViewCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
-        super.init(frame: fram)
+        super.init(frame: frame)
         configureUI()
         setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     func configureUI() {
