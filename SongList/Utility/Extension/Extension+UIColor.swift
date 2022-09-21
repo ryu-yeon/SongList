8a2eebe69c6bfa7f774340cf487e0f2b23cffde1
ryu-yeon <rytak108@gmail.com>
2022-09-21 18:25:09 +0900

    Extension+UIColor - String hex와 UIColor 상호작용하는 함수 생성

---
+++ SongList/Utility/Extension/Extension+UIColor.swift
@@ -0,0 +1,41 @@
+//
+//  Extension+UIColor.swift
+//  SongList
+//
+//  Created by 유연탁 on 2022/09/20.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init(hexFromString:String, alpha:CGFloat = 1.0) {
+        var cString:String = hexFromString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        var rgbValue:UInt32 = 10066329 //color #999999 if string has wrong format
+
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if ((cString.count) == 6) {
+            Scanner(string: cString).scanHexInt32(&rgbValue)
+        }
+
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: alpha
+        )
+    }
+    
+    func toHexString() -> String {
+            var r:CGFloat = 0
+            var g:CGFloat = 0
+            var b:CGFloat = 0
+            var a:CGFloat = 0
+            getRed(&r, green: &g, blue: &b, alpha: &a)
+            let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+
+            return String(format:"#%06x", rgb)
+        }
+}
