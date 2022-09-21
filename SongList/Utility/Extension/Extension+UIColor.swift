49fcb3915c189689779d651a13434870c9372aeb
ryu-yeon <rytak108@gmail.com>
2022-09-22 00:28:26 +0900

    코드 수정

--- SongList/Utility/Extension/Extension+UIColor.swift
+++ SongList/Utility/Extension/Extension+UIColor.swift
@@ -8,24 +8,22 @@
 import UIKit
 
 extension UIColor {
-    convenience init(hexFromString:String, alpha:CGFloat = 1.0) {
-        var cString:String = hexFromString.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-        var rgbValue:UInt32 = 10066329 //color #999999 if string has wrong format
-
-        if (cString.hasPrefix("#")) {
-            cString.remove(at: cString.startIndex)
+    convenience init(hexFromString: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hexFromString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
         }
-
-        if ((cString.count) == 6) {
-            Scanner(string: cString).scanHexInt32(&rgbValue)
-        }
-
-        self.init(
-            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-            alpha: alpha
-        )
+        
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+        
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+        
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
     }
     
     func toHexString() -> String {
