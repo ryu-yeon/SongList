43794239de540090d1e47e2013c77438c25fe8e3
ryu-yeon <rytak108@gmail.com>
2022-09-21 19:34:11 +0900

    DB에 List 저장 AddListView UI 수정

--- SongList/Scene/AddList/AddListView.swift
+++ SongList/Scene/AddList/AddListView.swift
@@ -10,23 +10,33 @@ import UIKit
 import SnapKit
 
 class AddListView: BaseView {
+        
+    let colorButton: UIButton = {
+        let view = UIButton()
+        view.layer.borderColor = UIColor.black.cgColor
+        view.layer.borderWidth = 1
+        view.layer.cornerRadius = 20
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    let colorTitleLabel: UILabel = {
+        let view = UILabel()
+        view.text = "리스트 커버 색상을 지정하세요."
+        view.font = .boldSystemFont(ofSize: 24)
+        return view
+    }()
     
     let titleLabel: UILabel = {
         let view = UILabel()
         view.text = "리스트에 이름을 지정하세요."
-        view.font = .boldSystemFont(ofSize: 28)
+        view.font = .boldSystemFont(ofSize: 24)
         return view
     }()
     
     let titleTextField: UITextField = {
         let view = UITextField()
-        view.placeholder = "내 리스트 #1"
-        return view
-    }()
-    
-    let cancelButton: UIButton = {
-        let view = UIButton()
-        view.setImage(UIImage(systemName: "xmark"), for: .normal)
+        view.borderStyle = .line
         return view
     }()
     
@@ -39,38 +49,45 @@ class AddListView: BaseView {
     
     override func configureUI() {
         self.backgroundColor = .systemBackground
-        [titleLabel, titleTextField, cancelButton, okButton].forEach {
+        [colorTitleLabel, colorButton, titleLabel, titleTextField, okButton].forEach {
             self.addSubview($0)
         }
     }
     
     override func setContraints() {
         
-        titleLabel.snp.makeConstraints { make in
+        colorTitleLabel.snp.makeConstraints { make in
             make.top.equalTo(self.safeAreaLayoutGuide).offset(40)
+            make.leading.trailing.equalTo(self).inset(20)
+            make.height.equalTo(28)
+        }
+        
+        colorButton.snp.makeConstraints { make in
+            make.top.equalTo(colorTitleLabel.snp.bottom).offset(20)
             make.centerX.equalTo(self)
-            make.height.equalTo(80)
+            make.width.equalTo(self).multipliedBy(0.4)
+            make.height.equalTo(colorButton.snp.width)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(colorButton.snp.bottom).offset(20)
+            make.leading.trailing.equalTo(self).inset(20)
+            make.height.equalTo(28)
         }
         
         titleTextField.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(20)
-            make.centerX.equalTo(self)
-            make.width.equalTo(self).multipliedBy(0.65)
-            make.height.equalTo(80)
-        }
-        
-        cancelButton.snp.makeConstraints { make in
-            make.top.equalTo(self.safeAreaLayoutGuide).offset(20)
-            make.trailing.equalTo(self.safeAreaLayoutGuide).offset(-20)
-            make.width.height.equalTo(40)
+            make.leading.trailing.equalTo(self).inset(20)
+            make.height.equalTo(48)
         }
         
         okButton.snp.makeConstraints { make in
             make.top.equalTo(titleTextField.snp.bottom).offset(20)
             make.centerX.equalTo(self)
-            make.height.equalTo(40)
             make.width.equalTo(120)
+            make.height.equalTo(36)
         }
+        
     }
 }
     
--- SongList/Scene/AddList/AddListViewController.swift
+++ SongList/Scene/AddList/AddListViewController.swift
@@ -7,9 +7,20 @@
 
 import UIKit
 
+import RealmSwift
+
 class AddListViewController: BaseViewController {
     
-    private let mainView = AddListView()
+    let mainView = AddListView()
+    
+    var listCount: Int?
+    
+    var colorString = ""
+    
+    let localRealm = try! Realm()
+    
+    var tasks: Results<ListRealm>!
+    
     
     override func loadView() {
         self.view = mainView
@@ -22,10 +33,34 @@ class AddListViewController: BaseViewController {
     
     override func configure() {
         mainView.okButton.addTarget(self, action: #selector(okButtonClicked), for: .touchUpInside)
+        mainView.colorButton.addTarget(self, action: #selector(colorButtonClicked), for: .touchUpInside)
+        mainView.titleTextField.placeholder = "내 리스트 #\(listCount ?? 0)"
+    }
+    
+    @objc func colorButtonClicked() {
+        let picker = UIColorPickerViewController()
+        picker.delegate = self
+        self.present(picker, animated: true, completion: nil)
     }
     
     @objc func okButtonClicked() {
         let vc = ListViewController()
+
+        if let title = mainView.titleTextField.text , title != "" {
+            ListRepository().saveList(title: title, color: colorString)
+        } else {
+            ListRepository().saveList(title: "내 리스트 #\(listCount ?? 0)", color: colorString)
+        }
+        tasks = localRealm.objects(ListRealm.self)
+        vc.task = tasks.last
         navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension AddListViewController: UIColorPickerViewControllerDelegate {
+    
+    func colorPickerViewController(_ viewController: UIColorPickerViewController, didSelect color: UIColor, continuously: Bool) {
+        mainView.colorButton.backgroundColor = color
+        colorString = color.toHexString()
     }
 }
