a02ba66561eca4bf7ed7b16f539e2556b2c03c9c
ryu-yeon <rytak108@gmail.com>
2022-09-27 07:36:58 +0900

    디바이스의 위치정보 가져오기

--- SongList/Scene/Map/MapViewController.swift
+++ SongList/Scene/Map/MapViewController.swift
@@ -15,6 +15,10 @@ class MapViewController: BaseViewController {
     
     var locationManager = CLLocationManager()
     
+    var list: [Karaoke] = []
+    
+    
+    
     override func loadView() {
         self.view = mainView
     }
@@ -23,18 +27,8 @@ class MapViewController: BaseViewController {
         super.viewDidLoad()
         
         locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        
-        
-        
-        if CLLocationManager.locationServicesEnabled() {
-            print("위치 서비스 On 상태")
-            locationManager.startUpdatingLocation()
-            print(locationManager.location?.coordinate)
-        } else {
-            print("위치 서비스 Off 상태")
-        }
+        checkUserDeviceLocationServiceAuthorization()
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -43,11 +37,67 @@ class MapViewController: BaseViewController {
     }
     
     override func configure() {
+        
         mainView.mapView.positionMode = .direction
         
     }
+    
+    func checkUserDeviceLocationServiceAuthorization() {
+        
+        let authorizationStatus: CLAuthorizationStatus
+        
+        authorizationStatus = locationManager.authorizationStatus
+        
+        if CLLocationManager.locationServicesEnabled() {
+            checkUserCurrentLocationAuthorization(authorizationStatus)
+        } else {
+            print("위치 서비스가 꺼져 있어서 위치 권한 요청을 못합니다.")
+        }
+    }
+    
+    func checkUserCurrentLocationAuthorization(_ authorizationStatus: CLAuthorizationStatus) {
+        switch authorizationStatus {
+        case .notDetermined:
+            print("NOTDETERMINED")
+            
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+
+        case .restricted, .denied:
+            print("DENIED") // 아이폰 설정으로 유도
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+            
+        default: print("DEFAULT")
+        }
+        
+    }
+    
 }
 
 extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let coordinate = locations.last?.coordinate {
+            let x = String(coordinate.longitude)
+            let y = String(coordinate.latitude)
+    
+            
+            KakaoAPIManager.shared.requestKaraoke(text: "노래방", x: x, y: y, radius: 500) { list in
+                self.list = list
+                dump(list)
+            }
+            
+            
+        }
+        
+        locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(#function)
+    }
     
 }
