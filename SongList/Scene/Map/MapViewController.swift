683455f8f26e63350f66dcbd502ed043305099bf
ryu-yeon <rytak108@gmail.com>
2022-10-01 03:37:36 +0900

    위치 권한 없을때 설정으로 이동 얼럿 생성 UI 수정

--- SongList/Scene/Map/MapViewController.swift
+++ SongList/Scene/Map/MapViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import CoreLocation
 
 import NMapsMap
+import Hero
 
 class MapViewController: BaseViewController {
     
@@ -21,7 +22,6 @@ class MapViewController: BaseViewController {
     let infoWindow = NMFInfoWindow()
     
     var url = ""
-
     
     override func loadView() {
         self.view = mainView
@@ -32,12 +32,8 @@ class MapViewController: BaseViewController {
         
         locationManager.delegate = self
         checkUserDeviceLocationServiceAuthorization()
-
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.isNavigationBarHidden = false
+        self.isHeroEnabled = true
+        mainView.mapLabel.heroID = "mapLabel"
     }
     
     override func configure() {
@@ -75,13 +71,20 @@ class MapViewController: BaseViewController {
             locationManager.startUpdatingLocation()
 
         case .restricted, .denied:
-            print("DENIED") // 아이폰 설정으로 유도
+            print("DENIED")
+            let alert = UIAlertController(title: "위치 권한이 필요합니다.", message: "개인정보 보호 및 보안-위치 서비스-뭐부르지 에서 위치 권한을 허용해주세요. ", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "확인", style: .destructive) { _ in
+                UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+            }
+            let cancel = UIAlertAction(title: "아니오", style: .cancel)
+            alert.addAction(ok)
+            alert.addAction(cancel)
+            present(alert, animated: true)
         case .authorizedWhenInUse:
             locationManager.startUpdatingLocation()
             
         default: print("DEFAULT")
         }
-        
     }
     
     @objc func urlButtonClicked() {
@@ -100,34 +103,47 @@ extension MapViewController: CLLocationManagerDelegate {
             let y = String(coordinate.latitude)
             
             let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: coordinate.latitude, lng: coordinate.longitude))
-                        cameraUpdate.animation = .easeIn
+            cameraUpdate.animation = .easeIn
             mainView.mapView.moveCamera(cameraUpdate)
             mainView.mapView.positionMode = .direction
-
-            KakaoAPIManager.shared.requestKaraoke(text: "노래방", x: x, y: y, radius: 500) { list in
-                self.list = list
-                dump(list)
-                for karaoke in list {
-                    let lat = Double(karaoke.y) ?? 0
-                    let lng = Double(karaoke.x) ?? 0
-                    let marker = NMFMarker(position: NMGLatLng(lat: lat, lng: lng))
-                    if karaoke.name.contains("코인") {
-                        marker.iconTintColor = UIColor.red
+            
+            KakaoAPIManager.shared.requestKaraoke(text: "노래방", x: x, y: y, radius: 1000, page: 1) { list, isEnd  in
+                self.list.append(contentsOf: list)
+                if isEnd == false {
+                    KakaoAPIManager.shared.requestKaraoke(text: "노래방", x: x, y: y, radius: 1000, page: 2) { list, isEnd  in
+                        self.list.append(contentsOf: list)
+                        for karaoke in self.list {
+                            let lat = Double(karaoke.y) ?? 0
+                            let lng = Double(karaoke.x) ?? 0
+                            let marker = NMFMarker(position: NMGLatLng(lat: lat, lng: lng))
+                            if karaoke.name.contains("코인") {
+                                marker.iconTintColor = UIColor.red
+                            }
+                            marker.mapView = self.mainView.mapView
+                            let handler = { [weak self] (overlay: NMFOverlay) -> Bool in
+                                let dataSource = NMFInfoWindowDefaultTextSource.data()
+                                if let marker = overlay as? NMFMarker {
+                                    if marker.infoWindow == nil {
+                                        dataSource.title = "선택"
+                                        self?.infoWindow.dataSource = dataSource
+                                        self?.infoWindow.open(with: marker)
+                                    } else {
+                                        self?.infoWindow.close()
+                                    }
+                                }
+                                self?.mainView.nameLabel.text = karaoke.name
+                                self?.mainView.distanceLabel.text = karaoke.distance + "m"
+                                self?.mainView.addressLabel.text = karaoke.address
+                                self?.mainView.urlButton.isHidden = false
+                                self?.url = karaoke.url
+                                return true
+                            }
+                            marker.touchHandler = handler
+                        }
                     }
-                    marker.mapView = self.mainView.mapView
-                    let handler = { [weak self] (overlay: NMFOverlay) -> Bool in
-                        self?.mainView.nameLabel.text = karaoke.name
-                        self?.mainView.distanceLabel.text = karaoke.distance + "m"
-                        self?.mainView.addressLabel.text = karaoke.address
-                        self?.mainView.urlButton.isHidden = false
-                        self?.url = karaoke.url
-                        return true
-                    }
-                    marker.touchHandler = handler
                 }
             }
         }
-        
         locationManager.stopUpdatingLocation()
     }
     
